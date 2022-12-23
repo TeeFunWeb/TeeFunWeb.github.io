fc5344abc6fb174dec5d94cb62976fd17c0192f7
Ninecloud2077 <ncjack2077@163.com>
2022-12-23 19:31:48 +0800

    导航栏及导入

--- libs/script.js
+++ libs/script.js
@@ -3,10 +3,25 @@ $(document).ready(function(){
         url:"http://teefunweb.github.io/libs/cdn.html",
         success:function(result){
             $("head").append(result);
-            $(document).foundation();
         }
-    })
+    });
+    $.ajax({
+        url:"http://teefunweb.github.io/libs/nav.html",
+        success:function(result){
+            $("body").prepend(result);
+        }
+    });
 
     var t=$('<title></title>').text(title+" | TeeFunWeb");
     $("head").append(t);
+
+    setTimeout(function(){
+        while(1){
+           try{
+                $(document).foundation();
+                break;
+            }catch{} 
+        } 
+    });
+    
 });
